f4053be44f2b7aebac8749722fc1eb68ed7ebeaa
ahmetkarsli <ahmet.karsli@icloud.com>
2024-09-03 23:45:15 +0200

    Initial Commit

---
+++ MealRecipes/MealRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealRecipesApp.swift
+//  MealRecipes
+//
+//  Created by Ahmet Karsli on 03.09.24.
+//
+
+import SwiftUI
+
+@main
+struct MealRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
